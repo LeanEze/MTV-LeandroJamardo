957bf2cb4b53bf9d6d50f89c8a9961cd237fbf5f
Leandro Jamardo <leandro.jamardo@gmail.com>
2022-07-03 19:42:36 -0300

    MTV entregable terminado

--- mi_app/models.py
+++ mi_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from unittest.util import _MAX_LENGTH
 
-# Create your models here.
+class Familiar(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    DNI = models.IntegerField(max_length=10)
+    edad = models.IntegerField(max_length=3)
--- mi_app/views.py
+++ mi_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from mi_app.models import Familiar
 
 def datos_familiares(request):
     context = {}
     context["familiares"] = Familiar.objects.all()#modelo
-    return render(request, "mi_app/datos_familiares.html", context)#templete
+    return render(request, "mi_app/index.html", context)#templete
